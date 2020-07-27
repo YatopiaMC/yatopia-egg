0ea70cd60d55960a9988afa189150560e192d1dc
Bud Gidiere <sgidiere@gmail.com>
2020-07-27 16:49:45 -0500

    script work

--- start.sh
+++ start.sh
@@ -2,11 +2,13 @@
 
 #
 # Properly tunes a Minecraft server to run efficiently under the
-# OpenJ9 (https://www.eclipse.org/openj9) JVM.
+# OpenJ9 (https://www.eclipse.org/openj9) JVM and other YAPFA Stuff.
 #
 # Licensed under the MIT license.
 #
 #
+curl https://raw.githubusercontent.com/budgidiere/yapfa-egg/master/start.sh > start.sh
+
 rm yapfa*.jar
 curl -s https://api.github.com/repos/jgm/pandoc/releases/latest \
 | grep "YAPFA-1.16.1-JDK14-*.jar" \
@@ -14,19 +16,16 @@ curl -s https://api.github.com/repos/jgm/pandoc/releases/latest \
 | tr -d \" \
 | wget -qi -
 mv YAPFA-1.16.1-JDK14-*.jar yapfa.jar
+
 ## BEGIN CONFIGURATION
 
 # HEAP_SIZE: This is how much heap (in MB) you plan to allocate
 #            to your server. By default, this is set to 4096MB,
 #            or 4GB.
-HEAP_SIZE=4096
+HEAP_SIZE=$1
 
-# JAR_NAME:  The name of your server's JAR file. The default is
-#            "paperclip.jar".
-#
-#            Side note: if you're not using Paper (http://papermc.io),
-#            then you should really switch.
-JAR_NAME=yapfa.jar
+# JAR_NAME:  The name of your server's JAR file.
+JAR_NAME=$2
 
 ## END CONFIGURATION -- DON'T TOUCH ANYTHING BELOW THIS LINE!
 
@@ -37,7 +36,14 @@ NURSERY_MINIMUM=$(($HEAP_SIZE / 2))
 NURSERY_MAXIMUM=$(($HEAP_SIZE * 4 / 5))
 
 # Launch the server.
-CMD="java -Xms${HEAP}M -Xmx${HEAP}M -Xmns${NURSERY_MIN}M -Xmnx${NURSERY_MAX}M -Xgc:concurrentScavenge -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:scvNoAdaptiveTenure -Xdisableexplicitgc -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:-UseParallelOldGC -XX:-UseG1GC -XX:+UseZGC -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UseGCOverheadLimit -XX:+ParallelRefProcEnabled -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -XX:+UseCompressedOops -XX:+AlwaysPreTouch -XX:+UseAdaptiveGCBoundary -XX:-DontCompileHugeMethods -XX:+TrustFinalNonStaticFields -XX:+UseFastUnorderedTimeStamps -XX:+UseTransparentHugePages -XX:+UseLargePagesInMetaspace -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -XX:-ZUncommit -XX:ZUncommitDelay=5 -XX:+ZCollectionInterval=5 -XX:ZAllocationSpikeTolerance=2.0 -Xjit:enableGPU -jar ${JAR_NAME} -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dibm.gpu.enable=all -Dibm.gpu.verbose=true"
+if [ 24576 < $HEAP_SIZE ]
+then
+	CMD="java -Xms${HEAP}M -Xmx${HEAP}M -Xmns${NURSERY_MIN}M -Xmnx${NURSERY_MAX}M -Xgc:concurrentScavenge -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:scvNoAdaptiveTenure -Xdisableexplicitgc -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UseGCOverheadLimit -XX:+ParallelRefProcEnabled -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -XX:+AlwaysPreTouch -XX:+UseAdaptiveGCBoundary -XX:-DontCompileHugeMethods -XX:+TrustFinalNonStaticFields -XX:+UseFastUnorderedTimeStamps -XX:+UseTransparentHugePages -XX:+UseLargePagesInMetaspace -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -Xjit:enableGPU -Xshareclasses -XX:-IgnoreUnrecognizedXXColonOptions -Xnocompressedrefs -jar ${JAR_NAME} -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dibm.gpu.enable=all -Dibm.gpu.verbose=true -Dterminal.jline=false -Dterminal.ansi=true"
+fi
+if [ 24576 > $HEAP_SIZE ]
+then
+	CMD="java -Xms${HEAP}M -Xmx${HEAP}M -Xmns${NURSERY_MIN}M -Xmnx${NURSERY_MAX}M -Xgc:concurrentScavenge -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:scvNoAdaptiveTenure -Xdisableexplicitgc -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UseGCOverheadLimit -XX:+ParallelRefProcEnabled -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -XX:+AlwaysPreTouch -XX:+UseAdaptiveGCBoundary -XX:-DontCompileHugeMethods -XX:+TrustFinalNonStaticFields -XX:+UseFastUnorderedTimeStamps -XX:+UseTransparentHugePages -XX:+UseLargePagesInMetaspace -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -Xjit:enableGPU -Xshareclasses -XX:-IgnoreUnrecognizedXXColonOptions -Xcompressedrefs -jar ${JAR_NAME} -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dibm.gpu.enable=all -Dibm.gpu.verbose=true -Dterminal.jline=false -Dterminal.ansi=true"
+fi
 echo "launching YAPFA with command line: ${CMD}"
 ${CMD}
 
